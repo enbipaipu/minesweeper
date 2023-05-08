79daafcf2750fa8e1c829e6bc3cf1e39c8a9b554
penbipaipu <s1f102300122@iniad.org>
2023-05-09 04:20:22 +0900

    途中 git commit -m 途中

---
+++ src/pages/index.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles:
+  & Readonly<{ "container": string }>
+  & Readonly<{ "board": string }>
+  & Readonly<{ "cell": string }>
+  & Readonly<{ "stone": string }>
+;
+export default styles;
+//# sourceMappingURL=./index.module.css.d.ts.map
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,168 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  //0 -> 未入力
+  //1 -> 左クリック
+  //2 -> 右クリック
+  //3 -> 旗
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const bombCount = 10;
+
+  //0->ボムなし
+  //1->ボムあり
+
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  );
+
+  //prett -ignore
+  const directions = [
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+  ];
+
+  const rClickStone = (x: number, y: number) => {
+    // 右クリックを消す
+    // setUserInputs(newUserInputs);
+    // document.getElementsByTagName('html')[0].oncontextmenu = function () {
+    //   return false;
+  };
+
+  const clickStone = (x: number, y: number) => {
+    console.log(x, y);
+    const newuserInputs = JSON.parse(JSON.stringify(userInputs));
+
+    if (board[x][y] === 0) {
+      newuserInputs[x][y] = 1;
+    }
+  };
+  //0 -> 未入力
+  //1 -> 左クリック
+  //2 -> 右クリック
+  //3 -> 旗
+
+  // -1 -> 石
+  // 0 -> 画像なしセル
+  //1~8 -> 数字セル
+  //9-> 石＋？
+  //10-> 石＋旗
+  //11-> ボムセル
+
+  //計算値
+  const board: number[][] = [];
+
+  for (let y = 0; y < 9; y++) {
+    const row: number[] = [];
+    for (let x = 0; x < 9; x++) {
+      row.push(0);
+    }
+    board.push(row);
+  }
+
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      if (userInputs[y][x] !== 0) {
+        // ユーザーの入力がある場合
+        board[y][x] = -1; // 石のセルを表す値
+      } else if (bombMap[y][x] === 1) {
+        // ボムがある場合
+        board[y][x] = 11; // ボムセルを表す値
+      } else {
+        // 周囲のボムの数を計算する
+        let bombCount = 0;
+        for (const [dx, dy] of directions) {
+          const newX = x + dx;
+          const newY = y + dy;
+          if (newX >= 0 && newX < 9 && newY >= 0 && newY < 9) {
+            if (bombMap[newY][newX] === 1) {
+              bombCount++;
+            }
+          }
+        }
+        board[y][x] = bombCount; // 数字セルの値を設定
+      }
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <div className={styles.icon} />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((val, x) => (
+            <div className={styles.cell} key={`${x}-${y}`}>
+              {val !== 3 &&
+                (val === 0 ? (
+                  <div
+                    className={styles.stone}
+                    onClick={() => clickStone(x, y)}
+                    onContextMenu={() => rClickStone(x, y)}
+                  />
+                ) : null)}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+// let zeroList: { x: number; y: number }[];
+// for () {
+//   zeroList = // board + directions + userInputs + bombMap
+// }
+
+// let openedCount: number
+// for () {
+//   openedCount = // board
+// }
+
+// const isSuccess = // openedCount + bombCount
+// let isFailure: boolean
+// for () {
+//   isFialure = // userInputs + bombMap
+// }
+
+// let isStarted: boolean
+// for () {
+//   isStarted = // userInputs
+// }
+
+// const addZeroAroundZero = (hoge: fuga) => {
+
+// }// 再帰関数
+
+// const reset = () => {
+
+// };
