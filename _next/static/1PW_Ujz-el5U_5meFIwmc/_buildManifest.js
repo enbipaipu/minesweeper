2e818ce4764fc8febea07702370cf0f91dc2057d
enbipaipu <enbipaipu@users.noreply.github.com>
2023-05-22 03:12:36 +0000

    deploy: 608bdaf08f8b89bc2643624dbf205c934adb0dbb

--- _next/static/1PW_Ujz-el5U_5meFIwmc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/516b19654a290395.css","static/chunks/pages/index-6c29f8f8eb1500a9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/y76lrrQvLIyS2tkYc9J41/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a9c212ad88ab7a90.css","static/chunks/pages/index-26710c283badd26b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
