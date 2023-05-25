90b070ec77903229851fb7768b70f4d403ae4d3e
enbipaipu <enbipaipu@users.noreply.github.com>
2023-05-25 11:55:18 +0000

    deploy: 5813518d41317861b6f60efe91e8d97ae0b74347

--- _next/static/3S56fSytLEBtJLdY1_Mow/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2b5683a1df150ec5.css","static/chunks/pages/index-11bb695ac64b766e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
